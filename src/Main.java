8ee976fa5d06e0164000b379d00d69c1e82f355e
Trishit Sanyal <trishitsanyal23@gmail.com>
2026-02-28 10:40:21 +0530

    refactor(UC3): replace string concatenation with String.join() to improve memory efficiency while preserving banner output

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-    System.out.println("  *****   " + "  *****   " + " ******** " + " ******** ");
-    System.out.println(" *     *  " + " *     *  " + " *       *" + " *        ");
-    System.out.println("*       * " + "*       * " + "*       * " + "*        ");
-    System.out.println("*       * " + "*       * " + " ******** " + " ******** ");
-    System.out.println("*       * " + "*       * " + "*         " + "        * ");
-    System.out.println(" *     *  " + " *     *  " + "*         " + "*       * ");
-    System.out.println("  *****   " + "  *****   " + "*         " + " ******** ");
+        System.out.println(String.join("", "  *****   ", "  *****   ", " ******** ", " ******** "));
+        System.out.println(String.join("", " *     *  ", " *     *  ", " *       *", " *        "));
+        System.out.println(String.join("", "*       * ", "*       * ", "*       * ", "*        "));
+        System.out.println(String.join("", "*       * ", "*       * ", " ******** ", " ******** "));
+        System.out.println(String.join("", "*       * ", "*       * ", "*         ", "        * "));
+        System.out.println(String.join("", " *     *  ", " *     *  ", "*         ", "*       * "));
+        System.out.println(String.join("", "  *****   ", "  *****   ", "*         ", " ******** "));
+
     }
 }
